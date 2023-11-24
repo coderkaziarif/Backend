589f07d41747a3f3b58d975d799645b10571e42f
kazi Arif <coderkaziarif@gmail.com>
2023-11-25 01:06:13 +0600

    now file deleted successfully form local folder

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,26 +1,36 @@
-import {v2 as cloudinary} from 'cloudinary';
-import fs from 'fs'
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-//* File upload on Cloudinary
-const uploadOnCloudinary = async (localFilepath)=> {
+
+const uploadOnCloudinary = async (localFilepath) => {
     try {
-        if(!localFilepath) return null
-        const res =  await cloudinary.uploader.upload(localFilepath,{
-            resource_type:"auto"
+        if (!localFilepath) return null;
+
+        const res = await cloudinary.uploader.upload(localFilepath, {
+            resource_type: 'auto',
         });
-        console.log("File is uploaded on cloudinary:",res.url)
+
+        // Log the Cloudinary upload response
+        console.log('File is uploaded on Cloudinary:', res.url);
+
         return res;
     } catch (error) {
-        fs.unlinkSync(localFilepath) //* its remove the local file when upload failed!
+        // Log the error
+        console.error('Error uploading to Cloudinary:', error);
+
         return null;
+    } finally {
+        // Delete the local file, whether upload succeeded or failed
+        if (localFilepath) {
+            fs.unlinkSync(localFilepath);
+            console.log('Local file deleted:', localFilepath);
+        }
     }
-  
-}
+};
 
-export {uploadOnCloudinary};
-
+export { uploadOnCloudinary };
